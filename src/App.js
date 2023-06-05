1aa52e664793fa9fce4e309ddc2cfd009fa9fb4b
KatyaTsvetkova <87665385+KatyaTsvetkova@users.noreply.github.com>
2023-06-05 22:20:24 +0300

    Adding Images Slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Find from "./components/find/Find";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero />
+      <Find/>
 
     </div>
   );
